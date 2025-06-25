55c9a0ce1aae77d30cdd40361259182cf4c9fb94
noemiemathu <152178258+noemiemathu@users.noreply.github.com>
2025-06-25 09:55:32 +0200

    Add files via upload

---
+++ assignment12/assignment12/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string[] listofnames = new string[5];
+Console.WriteLine($"Enter {listofnames.Length} names.");
+for (int i = 0; i < listofnames.Length; i++)
+{
+    Console.WriteLine($"Enter name {i + 1}");
+    listofnames[i] = Console.ReadLine();
+}
+
+foreach (string name in listofnames)
+{
+    Console.WriteLine(name);
+}
