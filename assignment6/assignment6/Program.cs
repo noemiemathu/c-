77dc2d806b6166f352246428d4bdb3dcbbba45cd
noemiemathu <152178258+noemiemathu@users.noreply.github.com>
2025-06-25 09:53:07 +0200

    Add files via upload

---
+++ assignment6/assignment6/Program.cs
@@ -0,0 +1,6 @@
+﻿
+        Console.WriteLine("Please Enter your Year of Birth:");
+        var today = DateTime.Today;
+        var birthday =  Console.ReadLine();
+        var age = today.Year - int.Parse(birthday);
+        Console.WriteLine("Your age is " + age + ".");
