4263621201d87467c622f7d6075f5d56ce728924
noemiemathu <152178258+noemiemathu@users.noreply.github.com>
2025-06-25 09:53:59 +0200

    Add files via upload

---
+++ assignment8/assignment8/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string word = "lucky";
+Console.WriteLine("What is my lucky word?");
+string word2 = Console.ReadLine();
+string word22 = word2.ToUpper();
+if (word == word2)
+{
+    Console.WriteLine("You typed LUCKY and you are... Correct!");
+}
+else
+{
+    Console.WriteLine("You typed " + (word22) + " and you are... Wrong!");
+}
