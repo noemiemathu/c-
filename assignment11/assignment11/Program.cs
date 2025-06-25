e109225c64424d236ecd2aa50e4de97a8168c2fc
noemiemathu <152178258+noemiemathu@users.noreply.github.com>
2025-06-25 09:54:57 +0200

    Add files via upload

---
+++ assignment11/assignment11/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("*** MENU ***");
+Console.WriteLine(" ");
+Console.WriteLine("1. Display Menu 1");
+Console.WriteLine("2. Display Menu 2");
+Console.WriteLine("3. Exit");
+Console.WriteLine("");
+while (true)
+{
+    ConsoleKeyInfo key = Console.ReadKey();
+    if (key.KeyChar == '1'|| key.KeyChar == '2' || key.KeyChar == '3') {
+        if (key.KeyChar == '1')
+        {
+            Console.WriteLine("Menu 1");
+        }
+        if (key.KeyChar == '2')
+        {
+            Console.WriteLine("Menu 2");
+        }
+        if (key.KeyChar == '3')
+        {
+            Console.WriteLine("Exit");
+        }
+    } else
+    {
+        Console.WriteLine("Invalid Selection. Please enter 1, 2 or 3");
+    }
+}
+
