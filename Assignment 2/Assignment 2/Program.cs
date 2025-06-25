9179e06f0d8b1033156315593bb823fb9cf2cdba
noemiemathu <152178258+noemiemathu@users.noreply.github.com>
2025-06-25 09:50:56 +0200

    Add files via upload

---
+++ Assignment 2/Assignment 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Welcome to C#!";
+            short b = 260;
+            int c = -56;
+            double d = 2001.99;
+            string e = "k1ngd0m";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assignement10/assignement10/Program.cs
@@ -0,0 +1,6 @@
+﻿
+for (int a = 1; a <= 10; a++)
+{
+    Console.WriteLine("The counter is {0}", a); 
+}
+
