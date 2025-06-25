af25aebf0fcd31fd8b5c2973837cb9f7c442e46d
noemiemathu <152178258+noemiemathu@users.noreply.github.com>
2025-06-25 09:51:44 +0200

    Add files via upload

---
+++ assignment3/assignment3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+
+int x = 6;
+string y = "2";
+
+string result1 = x.ToString() + y;
+int result2 = x + int.Parse(y);
+
+Console.WriteLine(result1);
+Console.WriteLine(result2);
+Console.ReadLine();
