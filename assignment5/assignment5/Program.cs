dde85d479f5ead41d2da265152696b925795be83
noemiemathu <152178258+noemiemathu@users.noreply.github.com>
2025-06-25 09:52:40 +0200

    Add files via upload

---
+++ assignment5/assignment5/Program.cs
@@ -0,0 +1,9 @@
+﻿
+double width;
+Console.WriteLine("please enter width");
+width = double.Parse(Console.ReadLine());
+double height;
+Console.WriteLine("please enter height");
+height = double.Parse(Console.ReadLine());
+double resultaat = Math.Round(width * height,2);
+Console.WriteLine(resultaat + " cm");
