0d3e8b5d611cbe6779f07f56ca841cb2ed3b8d00
noemiemathu <152178258+noemiemathu@users.noreply.github.com>
2025-06-25 09:54:26 +0200

    Add files via upload

---
+++ assignment9/assignment9/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Drink sizes: 1=small 2=medium 3=large");
+Console.WriteLine("Please enter your selection:");
+string userValue = Console.ReadLine();
+switch (userValue)
+{
+    case "1" or "small":
+        Console.WriteLine("Please insert 25 cents.");
+        Console.WriteLine("Thank you. Please come again.");
+        Console.ReadLine();
+        break;
+    case "2" or "medium":
+        Console.WriteLine("Please insert 50 cents.");
+        Console.WriteLine("Thank you. Please come again.");
+        Console.ReadLine();
+        break;
+    case "3" or "large":
+        Console.WriteLine("Please insert 75 cents.");
+        Console.WriteLine("Thank you. Please come again.");
+        Console.ReadLine();
+        break;
+    default:
+        Console.WriteLine("Invalid selection. Please enter 1, small, 2, medium, 3 or large.");
+        Console.ReadLine();
+        break;
+}
