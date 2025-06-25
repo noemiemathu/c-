5d57d31230a94bdd41990a0727920c39ec7bc089
noemiemathu <152178258+noemiemathu@users.noreply.github.com>
2025-06-25 09:52:13 +0200

    Add files via upload

---
+++ assignment4/assignment4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 6;
+if (a >= 1 && a <= 5)
+{
+    Console.WriteLine("The number is within range!");
+}
+else
+{
+    Console.WriteLine("The number is out of range!");
+}
+
