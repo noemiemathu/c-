f98e7f6c24c1b9d1b68dc86e2c08e73815aa7b4a
noemiemathu <152178258+noemiemathu@users.noreply.github.com>
2025-06-25 10:02:07 +0200

    Add files via upload

---
+++ AudioDevices1/AudioDevices1/Track.cs
@@ -0,0 +1,101 @@
+﻿
+namespace AudioDevices1.Tracks
+{
+    public class Track
+    {
+        private int id;
+        private string name;
+        private string artist;
+        private string albumSource;
+        private Category style;
+        private TimeSpan length;
+
+        enum Category
+        {
+            Ambient,
+            Blues,
+            Country,
+            Disco,
+            Electro,
+            Hardcore,
+            Hardrock,
+            HeavyMetal,
+            Hiphop,
+            Jazz,
+            Jumpstyle,
+            Klassiek,
+            Latin,
+            Other,
+            Pop,
+            Punk,
+            Reggae,
+            Rock,
+            Soul,
+            Trance,
+            Techno
+        }
+
+        public Track() { }
+        public Track(int id) { }
+        public Track(int id, string name) { }
+        public Track(int id, string artist, string name) { }
+
+        public TimeSpan GetLength()
+        {
+            return length;
+        }
+
+        public int GetLengthInSeconds()
+        {
+            return (int)length.TotalSeconds;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name 
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Artist 
+        {
+            get { return artist; } 
+            set { artist = value; }
+        }
+        public string DisplayName 
+        {
+            get 
+            {
+                if (name == null || artist == null)
+                {
+                    return "unknown";
+                }
+                else
+                {
+                    return $"{artist} - {name}";
+                } 
+            }
+        }
+        public TimeSpan Length
+        {
+            set { length = value; }
+        }
+        public string DisplayLengthb
+        {
+            get { return length.ToString(); }
+        }
+        public Category Style
+        {
+            get { return style; }
+            set { style = value; }
+        }
+        public string AlbumSource
+        {
+            get { return albumSource; }
+            set {  albumSource = value; }
+        }
+    }
+}
---
+++ AudioDevices1/Tester/Program.cs
@@ -0,0 +1,21 @@
+﻿using AudioDevices1.Tracks;
+
+namespace Tester
+{
+    namespace AudioDevices1.Tracks
+    {
+        public class Program
+        {
+            Track t1 = new Track(1, "Nelly Furtado", "Maneater");
+            t1.AlbumSource = "Loose";
+            t1.Length = new TimeSpan(0, 4, 31);
+            t1.Style = Category.Pop;
+
+            Console.WriteLine(t1.DisplayName);
+            Console.WriteLine(t1.DisplayLength);
+            Console.WriteLine("Tijd in seconden: {0}", t1.GetLengthInSeconds());
+            Console.WriteLine("Category: {0}", t1.Style.ToString());
+            Console.ReadLine();
+        }
+    }
+}
