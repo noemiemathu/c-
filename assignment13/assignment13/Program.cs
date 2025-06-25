b78d58a3388912dab41a43c640ff98b1032cdffe
noemiemathu <152178258+noemiemathu@users.noreply.github.com>
2025-06-25 09:56:14 +0200

    Add files via upload

---
+++ assignment13/assignment13/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int[] listnumbers = new int[5];
+Console.WriteLine("Enter 5 numbers.");
+
+for (int i = 0; i < listnumbers.Length; i++)
+{
+    Console.WriteLine($"number {i + 1}");
+
+    bool isNumber = int.TryParse(Console.ReadLine(), out int getal);
+    while (!isNumber)
+    {
+        Console.WriteLine("Please enter a number.");
+        isNumber = int.TryParse(Console.ReadLine(), out getal);
+    }
+
+        listnumbers[i] = getal;
+ 
+   
+}
+foreach(int i in listnumbers)
+{
+    Console.WriteLine(i);
+}
+
+
+
+
