72a05727ca0191a6e0a5aee20d3aebc0c3001119
noemiemathu <152178258+noemiemathu@users.noreply.github.com>
2025-06-25 09:53:32 +0200

    Add files via upload

---
+++ assignment7/assignment7/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+    Console.WriteLine("What is my lucky number?");
+    ConsoleKeyInfo keyName = new ConsoleKeyInfo();
+    keyName = Console.ReadKey();
+
+    //Console.WriteLine(keyName.KeyChar);
+    if (Char.IsNumber(keyName.KeyChar))
+{
+    if (keyName.KeyChar == '5')
+    {
+        Console.WriteLine(" Correct! My lucky number is 5!");
+    } else
+    {
+        Console.WriteLine(" is Incorrect");
+    }
+} else
+{
+    Console.WriteLine(" is incorrect. Please key in a number.");
+}
+
+
+
